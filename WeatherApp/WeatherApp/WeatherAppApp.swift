4801e5d622245a1c505325e88247a7a1e4dbd337
TomaszTwardowski <94967095+TomaszTwardowski@users.noreply.github.com>
2021-11-24 13:15:16 +0100

    add xcode template project

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Tomasz Twardowski on 24/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
